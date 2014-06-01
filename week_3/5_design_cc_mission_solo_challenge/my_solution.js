dadc2d6e0501af7653b0f70ed1653eaa9ffed104
dbc ariel <dbcariel14@gmail.com>
2014-06-01 19:23:17 -0400

    Update my_solution.js

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -1,18 +1,62 @@
+// JavaScript Document
 // This is a solo challenge
 
 // Your mission description:
-// 
-// 
-// 
+// Collect a Mushroom
+// kill an ogre
+// hand Mushroom to princess anya
 
 // Pseudocode
-// 
-// 
-// 
-// 
-// 
+// move player to the left by 10px
+// collect Mushroom
+// move player to the right by 10px
+// move player up by 10px
+// kill ogre
+// move up by 10px
+// hand Mushroom to princess anya
 
 // Initial Code
+var tharin = document.getElementById("Tharin");
+var anya = document.GetElementByID("anya");
+var enemy = document.GetElementById("enemy");
+var Mushroom = document.GetElementById("Mushroom");
+var positionX();
+var positionY();
+
+function moveLeft(){
+	tharin.PositionX += 10, 
+	tharin.PositionY = PositionY
+};
+function moveRight(){
+	tharin.PositionX -= 10px;
+	tharin.positionY = positionY
+};
+function moveUp(){
+	tharin.PositionX = PositionX;
+	tharin.positionY += 10px
+};
+function attackNearByEnemy(){
+	enemy = false
+};
+function CollectMushroom(){
+	tharin.addAttribute("Mushroom"),
+};
+function say(){
+};
+function giveMushroom(){
+	tharin.removeAttribute("Mushroom"),
+	anya.addAttribute("Mushroom")
+};
+
+
+this.moveLeft();
+this.CollectMushroom();
+this.moveRight();
+this.moveUp();
+this.attackNearByEnemy();
+this.moveUp();
+this.say("My Princess");
+this.giveMushroom();
 
 
 
@@ -27,9 +71,10 @@
 
 
 // Reflection
-// 
-// 
-// 
+// I failed the challenge. 
+// Wasn't expecting to dive into JS and writing functions so soon.
+// This called for moving figures on the screen to X, Y co-ordinates, adding and removing objects, 
+// I don't know how to express that in JS or any OO language. 
 // 
 // 
 // 
